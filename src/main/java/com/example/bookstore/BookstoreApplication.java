d40d998bede65081d78e9e5e356b4be7c7c58d07
SanteriLaivuori <santeri.laivuori@gmail.com>
2019-03-26 16:21:09 +0200

    Added bcrypt and user repository

--- src/main/java/com/example/bookstore/BookstoreApplication.java
+++ src/main/java/com/example/bookstore/BookstoreApplication.java
@@ -1,6 +1,5 @@
 package com.example.bookstore;
 
-import org.hibernate.annotations.common.util.impl.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
@@ -11,20 +10,21 @@ import com.example.bookstore.domain.Book;
 import com.example.bookstore.domain.BookRepository;
 import com.example.bookstore.domain.Category;
 import com.example.bookstore.domain.CategoryRepository;
-
-import ch.qos.logback.classic.Logger;
+import com.example.bookstore.domain.User;
+import com.example.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
 	@Autowired
 	private BookRepository repository;
-
+	@Autowired
+	private UserRepository urepository;
 	public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
 	}
 
 	@Bean
-	public CommandLineRunner Book(BookRepository repository, CategoryRepository crepository) {
+	public CommandLineRunner Book(BookRepository repository, CategoryRepository crepository, UserRepository urepository) {
 		return (args) -> {
 			
 			crepository.save(new Category("Comics"));
@@ -32,6 +32,11 @@ public class BookstoreApplication {
 			crepository.save(new Category("Trilogies"));
 			
 			repository.save(new Book("Keijo Koodari", "Superkoodari", "Testi1234", 1997, 19.99, crepository.findByName("Comics").get(0)));
+			
+			User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER");
+			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
+			urepository.save(user1);
+			urepository.save(user2);
 
 		};
 	}
